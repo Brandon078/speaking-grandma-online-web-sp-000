949bfe9fe2b9404313acc47b7f6b57ef78e121c1
brandon moro <brandon_078@hotmail.com>
2019-11-11 04:23:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,11 @@
 def speak_to_grandma(speak)
   if speak == 'I LOVE YOU GRANDMA!'
     return "I LOVE YOU TOO PUMPKIN"
-  elsif speak.upcase
-    return "NO, NOT SINCE 1938!"
-  else
+  end
+  if speak != speak.upcase
     return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
   end
 end
 
